c31f37bf956c664d4ae6b960387e787d4e67dc98
reute <j.rtl@outlook.de>
2019-07-17 15:27:30 +0200

    refactored

--- 13/chuck-a-luck/chuck-a-luck.py
+++ 13/chuck-a-luck/chuck-a-luck.py
@@ -3,34 +3,40 @@
 
 from random import randint
 
-# komm
-geld = 1000
-
 def wuerfeln():
 	return randint(1, 6)
+
+def berechneGewinn(wuerfe, zahl, einsatz):
+	gewinn = 0
+	for wurf in wuerfe:
+		if zahl == wurf:	# treffer			
+			gewinn += einsatz
+	if gewinn == 0:			# kein treffer
+		gewinn = -einsatz			
+	return gewinn
+
+geld = 1000
+anzahlWuerfeProRunde = 3
 
 print("**** Chuck-a-luck ****\nSie haben 1000 Geldeinheiten\nIn jeder Runde können Sie einen Teil davon auf eine der Zahlen 1 bis 6 setzen. Danach werden 3 Würfel geworfen. Falls Ihr Wert dabei ist, erhalten Sie Ihren Einsatz zurück und zusatzlich Ihren Einsatz fuer jeden Würfel,der die von Ihnen gesetzte Zahl zeigt.") 
 
 while geld > 0:
-	print(
-		"Sie haben " + str(geld) + " Geldeinheiten")
+	print("Sie haben " + str(geld) + " Geldeinheiten")
 	einsatz = int(input("Ihr Einsatz: "))
 	if einsatz == 0:
 		break
 	zahl = int(input("Ihre Zahl: "))	
 	print("Die Würfel sind gefallen:")
-	gewinn = 0
-	for i in range(3):
+	wuerfe = []
+	for i in range(anzahlWuerfeProRunde):
 		wurf = wuerfeln()
 		print(" " + str(wurf))
-		if zahl == wurf:
-			gewinn += einsatz	
-	if gewinn > 0:
-		geld += gewinn
+		wuerfe.append(wurf)
+	gewinn = berechneGewinn(wuerfe, zahl, einsatz)
+	if gewinn > 0:	
 		print("Glueckwunsch, Sie erhalten " + str(gewinn) + " Geldeinheiten!!")
-	else:
-		geld -= einsatz
+	else:	
 		print("Pech, da war nichts fuer Sie dabei!!")
-
+	geld += gewinn
 
 print("Glückwunsch, Sie verlassen das Casino mit " + str(geld) + " Geldeinheiten!!")
